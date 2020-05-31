0877d8e4e5b110a227dda3663f12221e2adaeb99
CoderXiaoLuo <471826078@qq.com>
2020-05-31 22:48:48 +0800

    评论

--- routes/comment.js
+++ routes/comment.js
@@ -3,3 +3,41 @@ const router = express.Router()
 const Comments = require('./../model/comment')
 const Users = require('./../model/user')
 const tokenConfig = require('./../utils/tokens')
+
+router.post('/addComment', (req, res, next) => {
+    const token = req.headers['authorization']
+    if (token) {
+        const { exp, id } = tokenConfig.verifyToken(token)
+        const dates = new Date()
+        const nowDate = parseInt(dates.getTime() / 1000)
+        if (exp > nowDate) {
+            Users.findById({ _id: id }, (err, doc) => {
+                if (err) {
+
+                } else {
+                    const userInfo = doc[0];
+                    const { articleId, recoveryId, content } = req.body
+                    if(recoveryId){
+                        Users.findById({_id: recoveryId},(err,docRecover)=>{
+                            if (err) {
+
+                            } else {
+                                const recoveryInfo = docRecover[0];
+
+                            }
+                        })
+                    }
+                    const newComments = new Comments({
+
+                    })
+                }
+
+            })
+
+        }
+    }
+})
+
+
+\
+
